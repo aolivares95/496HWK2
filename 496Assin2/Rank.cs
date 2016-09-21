10b183ca04f6cfb16e55eb78a96484cf2db3bbb8
Antonio Olivares <ao808779@wcupa.edu>
2016-09-21 16:44:55 -0400

    Added static constructor and list for rank values. Created instances of ranks within the static constructor. Created a constructor to modify private variables within the rank class.

--- 496Assin2/Rank.cs
+++ 496Assin2/Rank.cs
@@ -8,9 +8,39 @@ namespace _496Assin2
 {
     class Rank
     {
-        static int TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN;
-        static int KING, ACE;
-        List<int> values = new List<int> { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+        private string symbol;
+        private int val;
+        private string name;
+
+        static List<Rank> values= new List<Rank> { };
+
+        public Rank(string ns, int nv, string nn)
+        {
+            symbol = ns;
+            val = nv;
+            name = nn;
+        }
+
+        static Rank()
+        {
+
+            Rank ONE = new Rank("1", 1, "One");
+            Rank TWO = new Rank("2", 2, "Two");
+            Rank THREE = new Rank("3", 3, "Three");
+            Rank FOUR = new Rank("4", 4, "Four");
+            Rank FIVE = new Rank("5", 5, "Five");
+            Rank SIX = new Rank("6", 6, "Six");
+            Rank SEVEN = new Rank("7", 7, "Seven");
+            Rank EIGHT = new Rank("8", 8, "Eight");
+            Rank NINE = new Rank("9", 9, "Nine");
+            Rank TEN = new Rank("10", 10, "Ten");
+            Rank JACK = new Rank("J", 11, "Eleven");
+            Rank QUEEN = new Rank("Q", 12, "Queen");
+            Rank KING = new Rank("K",13,"King");
+            Rank ACE = new Rank("A", 14, "Ace");
+        }
+
+        
 
         int CompareTo(Rank rankObj)
         {
