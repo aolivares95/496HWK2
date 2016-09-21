b86ec88ec5ec6c3c04a66326d45a264879972222
Antonio Olivares <ao808779@wcupa.edu>
2016-09-21 15:33:44 -0400

    created method and static fields

---
+++ 496Assin2/Rank.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _496Assin2
+{
+    class Rank
+    {
+        static int TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN;
+        static int KING, ACE;
+        List<int> values = new List<int> { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+        int CompareTo(Rank rankObj)
+        {
+
+            return 0;
+        }
+     
+    }
+}
