ab1c580b53f6f1f991bb0a3bdc56bbff75a36da6
Antonio Olivares <ao808779@wcupa.edu>
2016-09-23 16:02:28 -0400

    Completed compareTo method, GetRank, toString and GetSuit

---
+++ 496Assin2/Card.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _496Assin2
+{
+    class Card
+    {
+       private Suit mySuit;
+       private Rank myRank;
+
+        public Card(Suit newSuit, Rank newRank)
+        {
+            mySuit = newSuit;
+           myRank = newRank;
+        }
+
+
+        public int CompareTo(Card newCard)
+        {
+            return this.myRank.CompareTo(newCard.myRank);
+        }
+        
+        public Rank GetRank()
+        {
+            return myRank;
+        }
+
+        public Suit GetSuit()
+        {
+
+            return mySuit;
+        }
+
+        public override string ToString()
+        {
+            return "Rank is: " + myRank + " Suit is: " + mySuit;
+
+        }
+
+    }
+}
