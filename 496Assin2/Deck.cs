c3f857f5dce428e76fec86d4308412082b44e5d0
Antonio Olivares <ao808779@wcupa.edu>
2016-09-23 16:00:55 -0400

    Added constructor to create deck, added DealOne method and AddCard method.

---
+++ 496Assin2/Deck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _496Assin2
+{
+    class Deck
+    {
+        static List<Card> deck;
+        Random rnd = new Random();
+        
+        public Deck()
+        {
+            deck= new List<Card> { };
+        }
+
+        public void AddCard(Card newCard)
+        {
+            deck.Add(newCard);
+        }
+
+        public Card DealOne()
+        {
+            int rnd1 = rnd.Next(0,deck.Count);
+            Card temp = deck[rnd1];
+            deck.RemoveAt(rnd1);
+            return temp;
+        }
+    }
+}
