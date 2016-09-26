ee8bfe75c014829aa78d7b2000f0147e4f17e668
Antonio Olivares <ao808779@wcupa.edu>
2016-09-25 22:43:26 -0400

    Created hand class added EvaluateHand, and CompareTo methods.

---
+++ 496Assin2/Hand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _496Assin2
+{
+    class Hand
+    {
+        List<Card> myHand;
+        public int sum=0;
+
+        public void AddCard(Card newCard)
+        {
+            myHand.Add(newCard);
+        }
+
+        public int CompareTo(Hand otherHand)
+        {
+            if(this.EvaluateHand()> otherHand.EvaluateHand())
+            {
+                return 1;
+            }
+
+            if (this.EvaluateHand() < otherHand.EvaluateHand())
+            {
+                return -1;
+            }
+
+            return 0;
+
+        }
+
+        public int EvaluateHand()
+        {
+            foreach(Card i in myHand)
+            {
+                sum = i.GetRank().val;
+            }
+            return sum;
+        }
+    }
+}
