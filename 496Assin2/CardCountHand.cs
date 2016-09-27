a1e361922b61e4e46baf1d9dc39132d428a7bcee
Antonio Olivares <ao808779@wcupa.edu>
2016-09-27 18:39:19 -0400

    Create cardcounthand class that implements the Hand superclass

---
+++ 496Assin2/CardCountHand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _496Assin2
+{
+    public class CardCountHand : Hand
+    {
+        
+        public override int CompareTo(Hand otherHand)
+        {
+            if (this.EvaluateHand() > otherHand.EvaluateHand())
+            {
+                return 1;
+            }
+
+            if (this.EvaluateHand() < otherHand.EvaluateHand())
+            {
+                return -1;
+            }
+
+            return 0;
+
+        }
+
+        public override int EvaluateHand()
+        {
+            int count = 0;
+            foreach (Card i in deck1.deck)
+            {
+                sum += deck1.deck[count].GetRank().val;
+            }
+
+            return sum;
+        }
+
+
+    }
+    
+}
