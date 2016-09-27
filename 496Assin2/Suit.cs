d60953484d42c1049144c5d7382359252868a5b3
Antonio Olivares <ao808779@wcupa.edu>
2016-09-27 19:32:22 -0400

    Changed methods and added static constructor

--- 496Assin2/Suit.cs
+++ 496Assin2/Suit.cs
@@ -6,11 +6,17 @@ using System.Threading.Tasks;
 
 namespace _496Assin2
 {
-    class Suit
+    public class Suit
     {
-        private string symbol;
-        private string name;
-        private int val;
+        public string symbol;
+        public string name;
+        public int val;
+        public static Suit CLUBS = new Suit();
+        public static Suit DIAMONDS = new Suit();
+        public static Suit HEARTS = new Suit();
+        public static Suit SPADES = new Suit();
+
+        public static List<Suit> values = new List<Suit> { };
 
         public Suit(string symbol2, string name2, int val2)
         {
@@ -19,11 +25,53 @@ namespace _496Assin2
             val = val2;
         }
 
-        static Suit(){
+        public Suit()
+        {
+
+        }
+
+       static Suit(){
             Suit CLUBS = new Suit("C","Club",1);
             Suit DIAMONDS = new Suit("D","Diamond",2);
             Suit HEARTS = new Suit("H", "Hearts", 3);
             Suit SPADES = new Suit("S", "Spades", 4);
+            values.Add(CLUBS);
+            values.Add(DIAMONDS);
+            values.Add(HEARTS);
+            values.Add(SPADES);
         }
+
+
+        public int CompareTo(Suit suitObj)
+        {
+            if (this.val < suitObj.val)
+            {
+                return -1;
+            }
+
+            if (this.val > suitObj.val)
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+        public string GetSymbol()
+        {
+            return this.symbol;
+        }
+
+        public string GetName()
+        {
+
+            return this.name;
+        }
+
+        public override string ToString()
+        {
+            return this.name;
+        }
+
+
     }
 }
