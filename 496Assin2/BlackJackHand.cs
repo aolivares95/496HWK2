ca93d0895c56a20c4ed3e20cec0313db50687793
Antonio Olivares <ao808779@wcupa.edu>
2016-09-27 18:40:17 -0400

    Created blackjackhand class that implements Hand

---
+++ 496Assin2/BlackJackHand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _496Assin2
+{
+    public class BlackJackHand : Hand
+    {
+        public override int CompareTo(Hand otherHand)
+        {
+            if (this.EvaluateHand() > otherHand.EvaluateHand())
+            {
+                return 1;
+            }
+            else if (this.EvaluateHand() < otherHand.EvaluateHand())
+            {
+                return -1;
+            }
+            else
+                return 0;
+        }
+
+
+        public override int EvaluateHand()
+        {
+            int count = 0;
+            foreach (Card i in deck1.deck)
+            {
+                sum += deck1.deck[count].GetRank().val;
+            }
+
+            return sum;
+        }
+
+      
+    }
+}
