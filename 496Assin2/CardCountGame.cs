d8587da8519ee68771db411a9f2a833a88d19690
Antonio Olivares <ao808779@wcupa.edu>
2016-09-27 18:37:49 -0400

    Created class that initializes Cardcounthand objects and adds cards to them

---
+++ 496Assin2/CardCountGame.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _496Assin2
+{
+    public class CardCountGame 
+    {
+        CardCountHand player1 = new CardCountHand();
+        CardCountHand player2 = new CardCountHand();
+
+        public void PlayGame()
+        {
+            Card rndCard = new Card();
+            Card rndCard2 = new Card();
+           // for (int i = 0; i < 8; i++)
+            //{
+
+                
+                player1.AddCard(rndCard);
+                player2.AddCard(rndCard2);
+                Console.WriteLine(rndCard.ToString()+" "+rndCard2.ToString());
+            //}
+
+        }
+
+
+    }
+}
